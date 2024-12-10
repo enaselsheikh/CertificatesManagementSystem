90013bc814690e0b11d757a6335b6edafd51d090
islamelbialy <islam.mohamed@mans.edu.eg>
2024-12-10 13:31:45 +0200

    2

--- certificates/admin.py
+++ certificates/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import CertificateSetting,CertificateType
 # Register your models here.
+
+admin.site.register(CertificateSetting)
+admin.site.register(CertificateType)
--- certificates/models.py
+++ certificates/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class CertificateType(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    desc = models.CharField(max_length=200,null=True,blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class CertificateSetting(models.Model):
+    imageLocationCoices = [(1,'اعلي اليمين'),(2,'اعلي اليسار')]
+    name = models.CharField(max_length=50,null=False,blank=False)
+    has_image = models.BooleanField(null=False)
+    image_location = models.CharField(max_length=25, choices=imageLocationCoices, default=1)
+    certificate_content = models.TextField(null=False,blank=False)
+    CertificateType = models.ForeignKey(CertificateType,related_name='CertificateType',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
