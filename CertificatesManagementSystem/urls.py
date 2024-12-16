e8a1b81e6156f8d86eacaf3bf0a34b772a24ad28
Islamelbialy <islam.mohamed@mans.edu.eg>
2024-12-16 12:55:55 +0200

    home

--- CertificatesManagementSystem/urls.py
+++ CertificatesManagementSystem/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('registeration.urls')),
     path('',include('Programs.urls')),
+    path('',include('certificates.urls')),
 ]
